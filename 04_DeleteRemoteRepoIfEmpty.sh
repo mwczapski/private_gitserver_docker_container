483353dc26a81f854a3dba8e35e72ba647f5e60d
mwczapski <57377978+mwczapski@users.noreply.github.com>
2020-05-17 11:27:54 +1000

    added 04_ and 05_

--- 04_DeleteRemoteRepoIfEmpty.sh
+++ 04_DeleteRemoteRepoIfEmpty.sh
@@ -2,7 +2,7 @@
 # #############################################
 # The MIT License (MIT)
 #
-# Copyright Â© 2020 Michael Czapski
+# Copyright © 2020 Michael Czapski
 # #############################################
 
 set -o pipefail
